bacc59b052da0e8907f210a6438ed16165c53c84
Michael Polzer <>
2018-10-03 22:51:20 +0200

    basic Ozziefication: datadir, conf & pid file

--- src/util.cpp
+++ src/util.cpp
@@ -81,8 +81,8 @@
 // Application startup time (used for uptime calculation)
 const int64_t nStartupTime = GetTime();
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
-const char * const BITCOIN_PID_FILENAME = "bitcoind.pid";
+const char * const BITCOIN_CONF_FILENAME = "ozziecoin.conf";
+const char * const BITCOIN_PID_FILENAME = "ozziecoind.pid";
 
 ArgsManager gArgs;
 
@@ -703,13 +703,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Ozziecoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Ozziecoin
+    // Mac: ~/Library/Application Support/Ozziecoin
+    // Unix: ~/.ozziecoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Ozziecoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -719,10 +719,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Ozziecoin";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".ozziecoin";
 #endif
 #endif
 }
